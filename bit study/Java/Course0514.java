e11395a4121bf32d74cba9ddd04752b828006463
wuli-nana <1485868146@qq.com>
2019-08-13 23:42:17 +0800

    未完成

---
+++ bit study/Java/Course0514.java
@@ -0,0 +1,44 @@
+
+
+
+public class Course0514
+{
+	public static void printIsGreatThan10(int n)
+	{
+		if(n > 10)
+		{
+			System.out.printf("%d 是大于10的%n",n);
+		}
+	}
+	public static void printColor(String color)
+	{
+		switch(color)
+		{
+			case "Red":
+				System.out.println("红色");
+				break;
+			case"Yellow":
+				System.out.println("黄色");
+				break;
+			default:
+				System.out.println("不知道是什么颜色");
+				break;
+		}
+	}
+	public string toString()
+	{
+		return ArrayTostring(ArrayTostring)
+	}
+	
+	public static void foreach(int [] array)
+	{
+		for (int )
+	}
+	public static void main( String[] args)
+	{
+		printGreaterThan(100);
+		printGreaterThan(9);
+		
+		printColor(args [0]);
+	}
+}
