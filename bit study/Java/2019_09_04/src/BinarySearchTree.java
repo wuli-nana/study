2e120dc01c332945cef1ba3aa61bec2d307faa4d
wuli-nana <1485868146@qq.com>
2019-09-06 21:43:58 +0800

    Map

---
+++ bit study/Java/2019_09_04/src/BinarySearchTree.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BinarySearchTree {
+    public static class Node{
+        int key;
+        int val;
+
+        Node left;
+        Node right;
+    }
+    private  Node root = null;
+
+    public int getOrfault(int key, int defaultValue) {
+        Node cur = root;
+        while(true){
+            if(root == null){
+                return -1;
+            }
+            if(){
+
+            }
+            List<String> list = new LinkedList<>();
+            if (defaultValue == key) {
+                list.add()
+            }
+            return -1;
+        }
+    }
+    public Set<Integer> keySet(){
+        Set<Integer> restult = new HashSet<>();
+        Queue<Node > queue = new LinkedList<>();
+        if(root == null){
+            return restult;
+
+        }
+        while(!queue.isEmpty()){
+
+        }
+    }
+}
---
+++ bit study/Java/2019_09_04/src/p2.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class p2 {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String,Integer> map = new HashMap<>();
+        for(String cp: cpdomains){
+            String[] t = cp.split(" ");
+            int n = Integer.valueOf(t[0]);
+            System.out.println(n);
+            String domain = t[1];
+
+            String[] u = domain.split("\\.");
+            for(int i = 0;i < u.length; i++){
+                String[] sub = Arrays.copyOfRange(u,i,u.length);
+                String d = String.join(".",sub);
+            }
+        }
+
+
+        return null;
+
+    }
+
+}
