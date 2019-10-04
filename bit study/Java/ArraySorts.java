4a094fa60760e100339843b751389de129abd412
wuli-nana <1485868146@qq.com>
2019-10-04 23:26:15 +0800

    数组随机抽取，插入排序改进

--- bit study/Java/ArraySorts.java
+++ bit study/Java/ArraySorts.java
@@ -1,7 +1,17 @@
 import java.util.Arrays;
+import java.util.Random;
 
-public class Sorts {
-    public static void insertSort(int array[]){
+public class ArraySorts {
+    public static int[] buildArray (int n){
+        Random random = new Random(2019102);
+        int[] array = new int[n];
+        for(int i = 0;i< n-1;i++){
+            array[i] = (random.nextInt())%100;
+        }
+        return array;
+    }
+    //插入排序
+    public static void insertSort1(int array[]){
         for(int i = 1; i < array.length-1;i++){
             int key = array[i +1];
             int j;
@@ -16,10 +26,24 @@ public class Sorts {
             array[j+1]= key;
         }
     }
+    //插入排序，改进循环。
+    public static void insertSort2(int[] array){
+        for(int i = 0;i< array.length - 1;i++){
+            int key = array[i + 1];
+            int j;
+            for(j = i;j > 0 && key < array[j];j--){
+                array[j+1]= array[j];
+            }
+            array[j+1]= key;
+        }
+    }
     public static void main(String[] args) {
-        int []array={1,2,4,5,7,8,2,4,7};
-        insertSort(array);
+        int[] array;
+        array = buildArray(20);
+        insertSort1(array);
         System.out.println(Arrays.toString(array));
+
+
 
     }
 }
